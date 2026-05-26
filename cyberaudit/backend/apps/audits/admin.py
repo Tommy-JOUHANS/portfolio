46df92ad01d136f0da2e129090063f2a675ec4bb
James0812 <james.roussel008@gmail.com>
2026-05-26 10:33:02 +0200

    add the code

--- cyberaudit/backend/apps/audits/admin.py
+++ cyberaudit/backend/apps/audits/admin.py
@@ -1,1 +1,17 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import AuditPack, AuditRequest
+
+
+@admin.register(AuditPack)
+class AuditPackAdmin(admin.ModelAdmin):
+    list_display = ("code", "name", "duration_days", "price")
+    search_fields = ("code", "name")
+
+
+@admin.register(AuditRequest)
+class AuditRequestAdmin(admin.ModelAdmin):
+    list_display = ("reference", "client", "pack", "status", "submitted_at")
+    list_filter = ("status", "pack")
+    search_fields = ("reference", "client__email")
+    readonly_fields = ("id", "reference", "submitted_at", "updated_at")
