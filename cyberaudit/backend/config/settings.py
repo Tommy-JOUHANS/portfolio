e41719ac797e31a87f40ec852047dd6f11897e82
Tommy JOUHANS <12119@holbertonstudents.com>
2026-05-27 09:47:33 +0200

    prolongement de la durée du Token

--- cyberaudit/backend/config/settings.py
+++ cyberaudit/backend/config/settings.py
@@ -86,7 +86,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=60),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "AUTH_HEADER_TYPES": ("Bearer",),
