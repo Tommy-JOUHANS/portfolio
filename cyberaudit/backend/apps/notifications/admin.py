3c7f214ca3e35449ee061c19c42474fb23bfe6fc
James0812 <james.roussel008@gmail.com>
2026-05-27 09:35:33 +0200

    add the code

--- cyberaudit/backend/apps/notifications/admin.py
+++ cyberaudit/backend/apps/notifications/admin.py
@@ -1,1 +1,11 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Notification
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ("type", "user", "subject", "status", "sent_at", "created_at")
+    list_filter = ("type", "status")
+    search_fields = ("subject", "user__email")
+    readonly_fields = ("id", "created_at", "sent_at")
